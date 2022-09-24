e795d95710a927de2dbd49a36e373acd0ec81cf0
BeBr0 <deniska_pavlov_2002@mail.ru>
2022-09-24 18:52:42 +0300

    Request message implemented

--- src/main/java/yt/bebr0/minecraftserverbot/bot/Bot.java
+++ src/main/java/yt/bebr0/minecraftserverbot/bot/Bot.java
@@ -5,6 +5,7 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -19,7 +20,7 @@ import java.util.UUID;
 
 public class Bot {
 
-    private static final Bot instance = new Bot();
+    private static Bot instance = new Bot();
 
     public static Bot getInstance() {
         return instance;
@@ -31,16 +32,53 @@ public class Bot {
     private final Guild guild = jda.getGuildById(Plugin.getInstance().getConfig().getLong("guild_id"));
     private TextChannel channel;
 
-    private List<VerificationManager.Request> requests = new ArrayList<>();
+    private final List<VerificationManager.Request> requests = new ArrayList<>();
 
     private Bot() {
         if (guild == null) {
-            Plugin.getInstance().getLogger().severe("Guild is NULL!");
+            Plugin.getInstance().getLogger().severe("Guild is NULL!");a
+            instance = null;
         }
         else {
             channel = guild.getTextChannelById(Plugin.getInstance().getConfig().getLong("channel_id"));
             if (channel == null) {
                 Plugin.getInstance().getLogger().severe("Channel is NULL!");
+                instance = null;
+            }
+        }
+    }
+
+    public void requestVerification(UUID requester, String requestedDiscordId) {
+        VerificationManager.Request request = VerificationManager.createRequest(requester, requestedDiscordId);
+
+
+
+        if (request != null) {
+            User user = jda.getUserById(requestedDiscordId);
+            Player player = Bukkit.getPlayer(requester);
+
+            assert user != null; // CHECKED PREVIOUSLY
+            assert player != null;
+            assert guild != null;
+
+            if (guild.getMemberById(requestedDiscordId) != null) {
+                user.openPrivateChannel()
+                        .flatMap(
+                                channel -> channel.sendMessage(
+                                        "```Вам был отправлен запрос на верификацию от дискорд сервера '" + guild.getName() + "'." +
+                                                " Запрос создан игроком " + player.displayName() + ". " +
+                                                "После принятия запроса он получит возможность слать сообщения в чат от вашего имени.\n\n" +
+                                                "\t❗️❗️ ЕСЛИ ВЫ НЕ СОЗДАВАЛИ ДАННЫЙ ЗАПРОС, ТО НАЖМИТЕ КРЕСТИК СНИЗУ ❗️❗\n\n️" +
+                                                "\t✅ Если запрос создали вы, нажмите галочку внизу```"
+                                )
+                        ).queue(
+                                message -> {
+                                    message.addReaction(Emoji.fromUnicode("✅"));
+                                    message.addReaction(Emoji.fromUnicode("❌"));
+                                }
+                        );
+
+                requests.add(request);
             }
         }
     }
